e5ded6b3f375e39165c58fa28894840aea185fb2
fsefu <fsefuwan@gmail.com>
2024-10-13 16:19:13 +0300

    Remove FlightDetails route and import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import FlightDetails from "./pages/FlightDetails";
 import Header from "./components/Header";
 // import Footer from "./components/Footer";
 
@@ -12,8 +11,6 @@ const App = () => {
         <main className="flex-grow container mx-auto px-4 py-6">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/flight/:id" element={<FlightDetails />} />
-            {/* Add more routes as needed */}
           </Routes>
         </main>
         {/* <Footer /> */}
